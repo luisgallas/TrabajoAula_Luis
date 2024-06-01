0a70df0a182b4de514ff8f0248badd91da313870
luisgallas <luisgallas.com@gmail.com>
2024-06-01 09:26:16 -0400

    Creacion de lista

--- Program.cs
+++ Program.cs
@@ -9,8 +9,31 @@ namespace TrabajoAula_Luis
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("hola");
+           //List<string> names = new List<string>();
+           List<string> names =  new List<string>();
+            //Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Add("Ana");
+            
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hola {name.ToUpper()}!");
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Bill a sido sacado de la lista\n");
+            Console.WriteLine("");
+            names.Remove("Bill");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            
+
             Console.ReadLine();
         }
     }
